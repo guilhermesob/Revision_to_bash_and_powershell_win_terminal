2e45c9e5cfe8ab79e82b07189fd5a1d98f44bd3e
guilhermesob <101477908+guilhermesob@users.noreply.github.com>
2023-07-10 19:06:07 -0300

    Create lembrete.sh

---
+++ lembrete.sh
@@ -0,0 +1,7 @@
+ #!/bin/bash
+
+while true; do
+    horaAtual=$(date +"%T")
+    echo "Lembre-se de separar os dentes e beber água! Hora atual: $horaAtual"
+    sleep 600  # Espera 10 minutos (600 segundos)
+done
